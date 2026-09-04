76eea20e7185250bdc8483f76b90eb466e324460
revaluate21 <coolpooptroop@gmail.com>
2026-09-04 14:12:08 +0100

    Add Lisbon Quest source manifest

---
+++ quest-data.js
@@ -0,0 +1,14 @@
+'use strict';
+window.__LQ_SOURCE_PARTS = [
+  './quest-src-01.txt?v=13',
+  './quest-src-02.txt?v=13',
+  './quest-src-03.txt?v=13',
+  './quest-src-04.txt?v=13',
+  './quest-src-05.txt?v=13',
+  './quest-src-06.txt?v=13',
+  './quest-src-07.txt?v=13',
+  './quest-src-08.txt?v=13',
+  './quest-src-09.txt?v=13',
+  './quest-src-10.txt?v=13',
+  './quest-src-11.txt?v=13'
+];
